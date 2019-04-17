530eecaa5b8ea92fab7e751fbe7cccd1b20ce280
TT DVGP <dvgp_admin@ViettelGroup.ViettelAD.com>
2019-04-17 14:23:27 +0700

    update index.html

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,7 @@
 import Home from '../components/pages/Home';
-<<<<<<< HEAD
-import ReadUser from '../components/pages/users/ReadUsers';
-import EditUser from '../components/pages/users/EditUser';
-// import AddUser from '../components/pages/users/AddUser';
-=======
 import ReadUsers from '../components/pages/users/ReadUsers';
 import EditUsers from '../components/pages/users/EditUser';
 import ReadPosts from '../components/pages/posts/ReadPosts';
->>>>>>> 0f925fb5acdbce2ddba0aa5da4db145ffb0848e4
 
 export const routes = [
     {
@@ -19,17 +13,12 @@ export const routes = [
         component: ReadUsers
     },
     {
-<<<<<<< HEAD
-        path: "/users/editUser",
-        component: EditUser
-=======
         path: "/posts/readPosts",
         component: ReadPosts
     },
     {
         path: "/users/editUser",
         component: EditUsers
->>>>>>> 0f925fb5acdbce2ddba0aa5da4db145ffb0848e4
     }
     // {
     //     path: "/users",
